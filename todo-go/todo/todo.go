15eec007d383d19ec0b5bf84e79d1ce3e7769aa2
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-06 21:18:51 +0100

    Add `Complete` and `Remove` methods

--- todo-go/todo/todo.go
+++ todo-go/todo/todo.go
@@ -1,6 +1,7 @@
 package todo
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -20,9 +21,35 @@ func (t *Todos) Add(todo_item Task) {
 	*t = append(*t, todo_item)
 }
 
+func (t *Todos) Complete(id int) error {
+	todo_list := *t
+	if id <= 0 || id > len(todo_list) {
+		return errors.New("Index out of range")
+	}
+
+	// The Todo ids use 1-based indexing, so need to subtract 1
+	todo_list[id-1].CompletedAt = time.Now()
+	todo_list[id-1].Done = true
+
+	return nil
+}
+
+func (t *Todos) Remove(id int) error {
+	todo_list := *t
+	if id <= 0 || id > len(todo_list) {
+		return errors.New("Index out of range")
+	}
+	*t = append(todo_list[:id-1], todo_list[id:]...)
+	return nil
+}
+
 func (t *Todos) Print() {
 	for i, item := range *t {
 		i++
-		fmt.Printf("%d - %s - Priority: %d\n", i, item.Description, item.Priority)
+		done_str := "false"
+		if item.Done {
+			done_str = "true"
+		}
+		fmt.Printf("%d - %s - Priority: %d - Done: %s\n", i, item.Description, item.Priority, done_str)
 	}
 }
