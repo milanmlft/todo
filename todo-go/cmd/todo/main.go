283a8bd41393d54966f3c359b1d70c683c35910f
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-09-06 21:28:36 +0100

    Test current methods

--- todo-go/cmd/todo/main.go
+++ todo-go/cmd/todo/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/milanmlft/todo/todo-go/todo"
 )
 
@@ -10,6 +12,18 @@ func main() {
 		Description: "Do this",
 		Priority:    2,
 	}
+	log.Println("Adding first todo")
 	todos.Add(task)
+	todos.Print()
+
+	log.Println("Marking first todo as complete")
+	todos.Complete(1)
+	todos.Print()
+
+	log.Println("Adding second todo")
+	todos.Add(todo.Task{Description: "Do that"})
+	todos.Print()
+	log.Println("Deleting second todo")
+	todos.Remove(2)
 	todos.Print()
 }
