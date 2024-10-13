7eddb60b8f4635ee6ce6fec7f68dfed65d63d5f3
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-13 17:42:07 +0100

    Initialise cobra

--- todo-go/main.go
+++ todo-go/main.go
@@ -1,29 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"log"
-
-	"github.com/milanmlft/todo/todo-go/todo"
-)
+import "github.com/milanmlft/todo/todo-go/cmd"
 
 func main() {
-	todos := &todo.Todos{}
-	task := todo.Task{
-		Description: "Do this",
-		Priority:    2,
-	}
-	log.Println("Adding first todo")
-	todos.Add(task)
-	todos.Print()
-
-	log.Println("Marking first todo as complete")
-	todos.Complete(1)
-	todos.Print()
-
-	log.Println("Adding second todo")
-	todos.Add(todo.Task{Description: "Do that"})
-	todos.Print()
-	log.Println("Deleting second todo")
-	todos.Remove(2)
-	todos.Print()
+	cmd.Execute()
 }
