70cf5eb88685eb9bd88e14643c05567d9390e78f
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-19 18:22:41 +0100

    Add tests for db reading and writing

--- todo-go/database/database_test.go
+++ todo-go/database/database_test.go
@@ -3,6 +3,8 @@ package database
 import (
 	"os"
 	"testing"
+
+	"github.com/milanmlft/todo/todo-go/todo"
 )
 
 func TestInitialiseDB(t *testing.T) {
@@ -28,5 +30,29 @@ func TestInitialiseDBErrors(t *testing.T) {
 	err := InitialiseDB("~/fail.json")
 	if err == nil {
 		t.Fatalf("InitialiseDB(\"~/fail.json\") gave nil error; want non-nill error")
+	}
+}
+
+func TestReadingAndWritingTodos(t *testing.T) {
+	dbpath, err := os.CreateTemp("", "tmp.json")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(dbpath.Name())
+
+	db := DatabaseHandler{path: dbpath.Name()}
+	origTodos := todo.Todos{todo.Task{Description: "todo"}}
+
+	err = db.WriteTodos(origTodos)
+	if err != nil {
+		t.Fatalf("db.WriteTodos(mockTodos) failed with %v; want nil", err)
+	}
+
+	readTodos, err := db.ReadTodos()
+	if len(readTodos) != len(origTodos) {
+		t.Fatalf("db.readTodos() returned %d todos, want %d", len(readTodos), len(origTodos))
+	}
+	if readTodos[0] != origTodos[0] {
+		t.Fatalf("db.ReadTodos() returned %v, want %v", readTodos, origTodos)
 	}
 }
