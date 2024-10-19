441be0becb5c31382091c496461269a7d7483359
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-19 15:03:39 +0100

    Implement `add` command

--- todo-go/cmd/add.go
+++ todo-go/cmd/add.go
@@ -1,28 +1,16 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/milanmlft/todo/todo-go/todo"
 	"github.com/spf13/cobra"
 )
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-	},
+	Short: "Add a new task",
+	Long:  `Add a new task to the todo list with the option to set a pririty level.`,
+	Run:   addRun,
 }
 
 func init() {
@@ -37,4 +25,13 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+	todos := todo.Todos{}
+	for _, arg := range args {
+		newTask := todo.Task{Description: arg}
+		todos.Add(newTask)
+	}
+	todos.Print()
 }
