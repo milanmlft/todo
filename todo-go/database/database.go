c9c7c2c484ee8947be2d64b5cd20d40817eb1f1d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-19 18:22:28 +0100

    Add database reading and writing

--- todo-go/database/database.go
+++ todo-go/database/database.go
@@ -3,6 +3,8 @@ package database
 import (
 	"encoding/json"
 	"os"
+
+	"github.com/milanmlft/todo/todo-go/todo"
 )
 
 func InitialiseDB(path string) error {
@@ -10,6 +12,31 @@ func InitialiseDB(path string) error {
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile(path, encoding, 0644)
-	return err
+	return os.WriteFile(path, encoding, 0644)
+}
+
+type DatabaseHandler struct {
+	path string
+}
+
+func (db *DatabaseHandler) ReadTodos() (todo.Todos, error) {
+	contents, err := os.ReadFile(db.path)
+	if err != nil {
+		return todo.Todos{}, err
+	}
+
+	var todos todo.Todos
+	err = json.Unmarshal(contents, &todos)
+	if err != nil {
+		return todo.Todos{}, err
+	}
+	return todos, nil
+}
+
+func (db *DatabaseHandler) WriteTodos(todoList todo.Todos) error {
+	encoding, err := json.Marshal(todoList)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(db.path, encoding, 0644)
 }
