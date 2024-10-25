866f80a6a22efc58e26befb8a8c1a3f5390272b8
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-25 22:03:51 +0100

    feat(go): add the `--all` flag for listing all tasks

--- todo-go/cmd/list.go
+++ todo-go/cmd/list.go
@@ -16,16 +16,23 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List current tasks",
 	Long: `Display the current set of tasks with their identifier,
-    description, priority, and status.
+    description, priority, and status. By default, this will only show
+    uncompleted tasks. Use the the --done or --all flags to show only
+    done or all tasks, respectively.
     `,
 	Run: listRun,
 }
 
-var doneOpt bool
+var (
+	doneOpt bool
+	allOpt  bool
+)
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-	listCmd.Flags().BoolVar(&doneOpt, "done", false, "Show 'Done' tasks")
+	listCmd.Flags().BoolVar(&doneOpt, "done", false, "Show only 'Done' tasks")
+	listCmd.Flags().BoolVar(&allOpt, "all", false,
+		"Show all tasks. Takes precedence over the --done flag")
 }
 
 func listRun(cmd *cobra.Command, args []string) {
@@ -39,7 +46,7 @@ func listRun(cmd *cobra.Command, args []string) {
 
 	writer := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
 	for _, task := range todos {
-		if task.Done == doneOpt {
+		if allOpt || task.Done == doneOpt {
 			fmt.Fprintln(writer, task.PrettyPrint())
 		}
 	}
