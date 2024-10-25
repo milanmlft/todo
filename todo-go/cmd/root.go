7ceea83e567b39b21cab4c43bb7c91ee08bc558c
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2024-10-25 23:09:06 +0100

    docs(go): Clean up main command description

--- todo-go/cmd/root.go
+++ todo-go/cmd/root.go
@@ -12,16 +12,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "todo-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Manage your tasks from teh command line",
 }
 
 var cfgFile string
